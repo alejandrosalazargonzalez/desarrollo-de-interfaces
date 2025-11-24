71c5d950190021191c419766d9da31b14bf6915b
alejandrosalazargonzalez <alejandrosalazargonzalez2004@gmail.com>
2025-11-24 15:29:45 +0000

    listo

--- ut-2/gestion_estado_navegacion/lib/main.dart
+++ ut-2/gestion_estado_navegacion/lib/main.dart
@@ -4,26 +4,74 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State createState() => _MyAppState();
+}
+
+class _MyAppState extends State {
+  bool _isDark = false;
+
+  void toggleTheme() {
+    setState(() {
+      _isDark = !_isDark;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Tema claro/oscuro',
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Home Page')),
+
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF0455BF),
+          brightness: Brightness.light,
+        ),
+      ),
+
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF0455BF),
+          brightness: Brightness.dark,
+        ),
+      ),
+
+      themeMode: _isDark ? ThemeMode.dark : ThemeMode.light,
+
+      home:HomePage(isDark: false,toggleTheme: () {
+        
+      },
+      )
+      );
+  }
+}
+
+class HomePage extends StatelessWidget{
+  final bool isDark;
+  final VoidCallback toggleTheme;
+  const HomePage({super.key, required this.isDark, required this.toggleTheme});
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+        appBar: AppBar(title: const Text('Tema claro / oscuro')),
 
         body: Center(
-          child: ElevatedButton(onPressed: () {}, child: Text('Botón')),
+          child: ElevatedButton(
+            onPressed: toggleTheme,
+            child: Text(isDark ? 'Cambiar a claro' : 'Cambiar a oscuro'),
+          ),
         ),
 
         floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Icon(Icons.light_mode),
+          onPressed: toggleTheme,
+          child: Icon(isDark ? Icons.light_mode : Icons.dark_mode),
         ),
-      ),
     );
-  }
+ }
 }
